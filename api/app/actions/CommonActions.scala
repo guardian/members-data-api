0f23d4c05fd4cc15fbb7fb9deb75002f9fe80e0b
Dan Massie <dan.massie.freelance@guardian.co.uk>
2015-07-29 12:56:37 +0100

    Remove unused imports from CommonActions

--- api/app/actions/CommonActions.scala
+++ api/app/actions/CommonActions.scala
@@ -2,11 +2,6 @@ package actions
 
 import actions.Functions._
 import controllers._
-import play.api.mvc.{Result, Request, ActionBuilder}
-import services.AuthenticationService
-import scala.concurrent.ExecutionContext.Implicits.global
-
-import scala.concurrent.Future
 
 trait CommonActions {
 
