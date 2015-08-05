f03311b5e4e9e5151209af0e4a64691dcfe7ffeb
Dan Massie <dan.massie.freelance@guardian.co.uk>
2015-08-05 11:43:48 +0100

    Remove unused properties and add healthcheck endpoint

--- provisioning/gu-identity-dist/membership-attributes-service-bootstrap.sh
+++ provisioning/gu-identity-dist/membership-attributes-service-bootstrap.sh
@@ -4,7 +4,6 @@ source set-env.sh
 
 adduser --home /$apptag-1.0-SNAPSHOT --disabled-password --gecos \"\" $apptag
 
-aws s3 cp s3://gu-membership-attributes-service-dist/$stagetag/application.properties /etc/gu/membership-attributes-service.properties
 aws s3 cp s3://gu-membership-attributes-service-dist/upstart.conf /etc/init/$apptag.conf
 aws s3 cp s3://gu-membership-attributes-service-dist/$stacktag/$stagetag/$apptag/$apptag-1.0-SNAPSHOT.tgz /$apptag-1.0-SNAPSHOT/$apptag-1.0-SNAPSHOT.tgz
 tar -xvzf /$apptag-1.0-SNAPSHOT/$apptag-1.0-SNAPSHOT.tgz
