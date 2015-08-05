221c5a0334c69c62b13e5a43a214bd95c81e24c9
Dan Massie <dan.massie.freelance@guardian.co.uk>
2015-08-05 12:05:50 +0100

    Fix typos in s3 buckets

--- provisioning/gu-identity-dist/membership-attributes-service-bootstrap.sh
+++ provisioning/gu-identity-dist/membership-attributes-service-bootstrap.sh
@@ -4,8 +4,8 @@ source set-env.sh
 
 adduser --home /$apptag-1.0-SNAPSHOT --disabled-password --gecos \"\" $apptag
 
-aws s3 cp s3://gu-membership-attributes-service-dist/upstart.conf /etc/init/$apptag.conf
-aws s3 cp s3://gu-membership-attributes-service-dist/$stacktag/$stagetag/$apptag/$apptag-1.0-SNAPSHOT.tgz /$apptag-1.0-SNAPSHOT/$apptag-1.0-SNAPSHOT.tgz
+aws s3 cp s3://gu-membership-attribute-service-dist/upstart.conf /etc/init/$apptag.conf
+aws s3 cp s3://gu-membership-attribute-service-dist/$stacktag/$stagetag/$apptag/$apptag-1.0-SNAPSHOT.tgz /$apptag-1.0-SNAPSHOT/$apptag-1.0-SNAPSHOT.tgz
 tar -xvzf /$apptag-1.0-SNAPSHOT/$apptag-1.0-SNAPSHOT.tgz
 chown -R $apptag /$apptag-1.0-SNAPSHOT
 sed -i "s/<APP>/$apptag/g" /etc/init/$apptag.conf
