ca640ead4c4fc6ae0ff9187f6e1924538d2c5946
Dan Massie <dan.massie.freelance@guardian.co.uk>
2015-08-05 15:28:51 +0100

    Almost working..

---
+++ provisioning/gu-identity-dist/membership-attribute-service-bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source set-env.sh
+
+adduser --home /$apptag --disabled-password --gecos \"\" $apptag
+
+aws s3 cp s3://gu-membership-attribute-service-dist/upstart.conf /etc/init/$apptag.conf
+aws s3 cp s3://gu-membership-attribute-service-dist/$stacktag/$stagetag/$apptag/app.zip /$apptag/$apptag.zip
+apt-get -y install unzip
+
+unzip /$apptag/$apptag.zip -d /$apptag
+chown -R $apptag /$apptag
+sed -i "s/<APP>/$apptag/g" /etc/init/$apptag.conf
