bad2a630e8733a4170bd2cb86fd1f18366822db8
Dan Massie <dan.massie.freelance@guardian.co.uk>
2015-08-06 14:06:05 +0100

    Remove install unzip as packer now does this

--- provisioning/gu-identity-dist/membership-attribute-service-bootstrap.sh
+++ provisioning/gu-identity-dist/membership-attribute-service-bootstrap.sh
@@ -6,7 +6,6 @@ adduser --home /$apptag --disabled-password --gecos \"\" $apptag
 
 aws s3 cp s3://gu-membership-attribute-service-dist/upstart.conf /etc/init/$apptag.conf
 aws s3 cp s3://gu-membership-attribute-service-dist/$stacktag/$stagetag/$apptag/app.zip /$apptag/$apptag.zip
-apt-get -y install unzip
 
 unzip /$apptag/$apptag.zip -d /$apptag
 chown -R $apptag /$apptag
