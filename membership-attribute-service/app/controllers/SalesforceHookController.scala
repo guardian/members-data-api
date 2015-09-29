8e67dee39715b77310af63e164221f0c61a3b6ef
tudorraul <tudorraul@gmail.com>
2015-09-29 12:59:07 +0100

    add salesforce hook update metric

--- membership-attribute-service/app/controllers/SalesforceHookController.scala
+++ membership-attribute-service/app/controllers/SalesforceHookController.scala
@@ -3,6 +3,7 @@ package controllers
 import actions.SalesforceAuthAction
 import com.google.inject.Inject
 import models.ApiErrors
+import monitoring.CloudWatch
 import parsers.{Salesforce => SFParser}
 import play.Logger
 import play.api.libs.concurrent.Execution.Implicits._
@@ -14,9 +15,14 @@ import scala.concurrent.Future
 import scalaz.{-\/, \/-}
 
 class SalesforceHookController @Inject() (attrService: AttributeService) {
+
+  val metrics = CloudWatch("SalesforceHookController")
+
   def createAttributes = SalesforceAuthAction.async(parse.xml) { request =>
     SFParser.parseOutboundMessage(request.body) match {
-      case \/-(attrs) => attrService.setAttributes(attrs).map(const(attrs))
+      case \/-(attrs) =>
+        metrics.put("Update", 1)
+        attrService.setAttributes(attrs).map(const(attrs))
       case -\/(msg) =>
         Logger.error(s"Web hook payload unrecognised ${request.body}")
         Future(ApiErrors.badRequest(msg))
