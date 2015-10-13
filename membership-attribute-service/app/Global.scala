a75265862fd7c91cfb5195e528bfc850c5c2f33d
Thomas Franquelin <ostapneko@gmail.com>
2015-10-13 12:09:39 +0100

    Initialize Sentry on startup

--- membership-attribute-service/app/Global.scala
+++ membership-attribute-service/app/Global.scala
@@ -1,6 +1,7 @@
 import filters.{AddEC2InstanceHeader, LogRequestsFilter}
 import models.ApiErrors._
-import play.api.Logger
+import monitoring.SentryLogging
+import play.api.{Application, Logger}
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc.{RequestHeader, Result, WithFilters}
 import play.filters.cors.CORSFilter
@@ -12,6 +13,10 @@ import scala.concurrent.Future
 object Global extends WithFilters(CSRFFilter(), AddEC2InstanceHeader, LogRequestsFilter, CORSFilter(corsConfig)) {
 
   private val logger = Logger(this.getClass)
+
+  override def onStart(app: Application) {
+    SentryLogging.init()
+  }
 
   override def onBadRequest(request: RequestHeader, error: String): Future[Result] = {
     logger.debug(s"Bad request: $request, error: $error")
