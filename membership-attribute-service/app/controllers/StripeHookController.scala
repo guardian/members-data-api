e29942af30491009ab3cd2180cf0e8a5f1e20f64
tverran <tom.verran@guardian.co.uk>
2016-04-07 14:38:06 +0100

    Update identity rather than salesforce with the webhook

--- membership-attribute-service/app/controllers/StripeHookController.scala
+++ membership-attribute-service/app/controllers/StripeHookController.scala
@@ -2,13 +2,11 @@ package controllers
 
 import com.typesafe.scalalogging.LazyLogging
 import play.api.libs.concurrent.Execution.Implicits._
-import com.gu.salesforce.ContactDeserializer.Keys
 import com.gu.stripe.Stripe._
 import com.gu.stripe.Stripe.Deserializer._
 import components.{TestTouchpointComponents, NormalTouchpointComponents}
 import scalaz.std.scalaFuture._
 import play.api.libs.json.Json
-import play.api.mvc.Results._
 import play.api.mvc._
 import scala.concurrent.Future
 import scalaz.OptionT
@@ -24,11 +22,11 @@ class StripeHookController extends Controller with LazyLogging {
         e <- OptionT(Future.successful(event.asOpt))
         tp = if(e.liveMode) NormalTouchpointComponents else TestTouchpointComponents
         eventFromStripe <- OptionT(tp.giraffeStripeService.Event.findCharge(e.id))
-        contact <- OptionT(tp.contactRepo.getByEmail(eventFromStripe.`object`.receipt_email))
+        identityId <- OptionT(tp.identityService.user(eventFromStripe.`object`.receipt_email))
         allowMarketing <- OptionT(Future.successful(eventFromStripe.`object`.metadata.get("marketing-opt-in").map(_ == "true")))
       } yield {
-        tp.contactRepo.upsert(contact.identityId, Json.obj(Keys.ALLOW_GU_RELATED_MAIL -> allowMarketing))
-        logger.info(s"${contact.identityId} marketing -> $allowMarketing")
+        tp.identityService.setMarketingPreference(identityId, allowMarketing)
+        logger.info(s"$identityId marketing -> $allowMarketing")
       }).run
       .map(_.fold[Result](Ok(Json.obj("updated" -> false)))
                          (_ => Ok(Json.obj("updated" -> true))))
