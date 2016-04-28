f815917680bf7704f68ace556707740701b62bf3
tverran <tom.verran@guardian.co.uk>
2016-04-28 14:41:08 +0100

    Add more logging

--- membership-attribute-service/app/controllers/AttributeController.scala
+++ membership-attribute-service/app/controllers/AttributeController.scala
@@ -1,5 +1,6 @@
 package controllers
 import com.gu.memsub._
+import com.typesafe.scalalogging.LazyLogging
 import configuration.Config
 import models.ApiError._
 import models.ApiErrors._
@@ -9,32 +10,41 @@ import models._
 import monitoring.CloudWatch
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.json.Json
-import play.api.mvc.Result
+import play.api.mvc.{Controller, Result}
 import _root_.services.{AuthenticationService, IdentityAuthService}
 import play.filters.cors.CORSActionBuilder
 import scala.concurrent.Future
 import scalaz.{\/, EitherT}
 import scalaz.std.scalaFuture._
-import play.api.mvc.Results.Ok
 import scalaz.syntax.std.option._
 
 
-class AttributeController {
+class AttributeController extends Controller with LazyLogging {
 
   lazy val corsFilter = CORSActionBuilder(Config.corsConfig)
   lazy val backendAction = corsFilter andThen BackendFromCookieAction
   lazy val authenticationService: AuthenticationService = IdentityAuthService
   lazy val metrics = CloudWatch("AttributesController")
 
-  def update() = BackendFromCookieAction.async { implicit request =>
-    (for {
+  def update = BackendFromCookieAction.async { implicit request =>
+
+    val result: EitherT[Future, String, Attributes] = for {
       id <- EitherT(Future.successful(authenticationService.userId \/> "No user"))
       contact <- EitherT(request.touchpoint.contactRepo.get(id).map(_ \/> s"No contact for $id"))
       sub <- EitherT(request.touchpoint.membershipSubscriptionService.get(contact)(Membership).map(_ \/> s"No sub for $id"))
-      res <- EitherT(request.touchpoint.attrService.set(Attributes(id, sub.plan.tier.name, contact.regNumber)).map(\/.right))
-    } yield res).run.map(_.fold(
-      error => ApiErrors.badRequest(error),
-      _ => Ok(Json.obj("updated" -> true))
+      attributes = Attributes(id, sub.plan.tier.name, contact.regNumber)
+      res <- EitherT(request.touchpoint.attrService.set(attributes).map(\/.right))
+    } yield attributes
+
+    result.run.map(_.fold(
+      error => {
+        logger.error(s"Failed to update attributes - $error")
+        ApiErrors.badRequest(error)
+      },
+      attributes => {
+        logger.info(s"${attributes.userId} -> ${attributes.tier}")
+        Ok(Json.obj("updated" -> true))
+      }
     ))
   }
 
