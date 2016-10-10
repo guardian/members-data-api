b0eb66b3ae69960300b1b02a4ed9e4e7a769650f
Jacob Winch <jacob.winch@guardian.co.uk>
2016-10-10 10:43:45 +0100

    Update membership-common version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val awsDynamo = "com.amazonaws" % "aws-java-sdk-dynamodb" % awsClientVersion
   val awsSNS = "com.amazonaws" % "aws-java-sdk-sns" % awsClientVersion
   val awsCloudWatch = "com.amazonaws" % "aws-java-sdk-cloudwatch" % awsClientVersion
-  val membershipCommon = "com.gu" %% "membership-common" % "0.282"
+  val membershipCommon = "com.gu" %% "membership-common" % "0.285"
   val scalaz = "org.scalaz" %% "scalaz-core" % "7.1.1"
 
   //projects
