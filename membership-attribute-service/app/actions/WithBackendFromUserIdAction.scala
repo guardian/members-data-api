b61e7acadba8d53d6148d2da11f796b9affef17a
Santiago Villa Fernandez <santiago.villa-fernandez@guardian.co.uk>
2017-06-14 14:40:09 +0100

    Added support for testUser query string in update endopoint

--- membership-attribute-service/app/actions/WithBackendFromUserIdAction.scala
+++ membership-attribute-service/app/actions/WithBackendFromUserIdAction.scala
@@ -9,10 +9,8 @@ import configuration.Config
 
 object WithBackendFromUserIdAction extends ActionRefiner[Request, BackendRequest] {
   override protected def refine[A](request: Request[A]): Future[Either[Result, BackendRequest[A]]] = Future {
-    val firstName = IdentityAuthService.username(request).flatMap(_.split(' ').headOption) //Identity checks for test users by first name
-    val exists = firstName.exists(Config.testUsernames.isValid)
-
-    val backendConf = if (exists) {
+    val testUser : Boolean = request.getQueryString("testUser").contains("true");
+    val backendConf = if (testUser) {
       TestTouchpointComponents
     } else {
       NormalTouchpointComponents
