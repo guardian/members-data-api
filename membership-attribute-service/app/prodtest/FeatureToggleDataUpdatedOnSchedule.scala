467db612773531119a6e269ac518b1f475f3b9b0
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2017-07-31 10:39:48 +0100

    Feature toggle service gets a disjunction rather than an option

--- membership-attribute-service/app/prodtest/FeatureToggleDataUpdatedOnSchedule.scala
+++ membership-attribute-service/app/prodtest/FeatureToggleDataUpdatedOnSchedule.scala
@@ -5,8 +5,9 @@ import com.gu.memsub.util.ScheduledTask
 import com.typesafe.scalalogging.LazyLogging
 import services.ScanamoFeatureToggleService
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.DurationInt
+import scalaz.{-\/, \/-}
 
 class FeatureToggleDataUpdatedOnSchedule(featureToggleService: ScanamoFeatureToggleService)(implicit ec: ExecutionContext, system: ActorSystem) extends LazyLogging {
 
@@ -15,17 +16,20 @@ class FeatureToggleDataUpdatedOnSchedule(featureToggleService: ScanamoFeatureTog
       updatePercentage("UpdateAttributesFromZuoraLookupPercentage")
     }
 
-  private def updatePercentage(featureName: String): Future[Int] = featureToggleService.get("AttributesFromZuoraLookup").map {
-    case Some(feature) =>
-      val percentage = feature.TrafficPercentage.getOrElse(0)
-      logger.info(s"$featureName scheduled task set percentage to $percentage")
-      updateZuoraTrafficPercentageTask.agent.alter(percentage)
-      percentage
-    case None =>
-      logger.info(s"Tried to update the percentage of traffic for $featureName, but that feature was not " +
-      "found in the table. Setting traffic to 0%")
-      updateZuoraTrafficPercentageTask.agent.alter(0)
-      0
+  private def updatePercentage(featureName: String) = featureToggleService.get("AttributesFromZuoraLookup") map { result =>
+    result match {
+       case \/-(feature) =>
+         val percentage = feature.TrafficPercentage.getOrElse(0)
+         logger.info(s"$featureName scheduled task set percentage to $percentage")
+         updateZuoraTrafficPercentageTask.agent.alter(percentage)
+         percentage
+       case -\/(e) =>
+         logger.warn(s"Tried to update the percentage of traffic for $featureName, but that feature was not " +
+           s"found in the table. Setting traffic to 0%. Error: $e")
+         updateZuoraTrafficPercentageTask.agent.alter(0)
+         0
+    }
+
   }
 
   updateZuoraTrafficPercentageTask.start()
--- membership-attribute-service/app/services/ScanamoFeatureToggleService.scala
+++ membership-attribute-service/app/services/ScanamoFeatureToggleService.scala
@@ -9,17 +9,24 @@ import models.FeatureToggle
 import play.api.libs.concurrent.Execution.Implicits._
 
 import scala.concurrent.Future
+import scalaz.\/
+import scalaz.syntax.std.either._
+import scalaz.syntax.std.option._
 
 class ScanamoFeatureToggleService(client: AmazonDynamoDBAsyncClient, table: String) extends LazyLogging {
 
   private val scanamo = Table[FeatureToggle](table)
   def run[T] = ScanamoAsync.exec[T](client) _
 
-  def get(featureName: String): Future[Option[FeatureToggle]] =
-    run(scanamo.get('FeatureName -> featureName).map(_.flatMap {
-      _
-        .left.map(e => logger.warn(s"Scanamo error in get: ${DynamoReadError.describe(e)}", e))
-        .right.toOption
-    }))
+  def get(featureName: String): Future[\/[String, FeatureToggle]] =
+    run(scanamo.get('FeatureName -> featureName).map { maybeFeatureToggle =>
+      val featureToggleDisjunction = maybeFeatureToggle map { featureToggleResult =>
+        featureToggleResult.disjunction.leftMap(DynamoReadError.describe)
+      }
 
+      val featureToggle = featureToggleDisjunction \/> "Feature toggle not found"
+
+      featureToggle.flatMap(identity)
+    }
+    )
 }
