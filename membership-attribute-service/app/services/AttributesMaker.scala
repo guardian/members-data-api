b45872ac9d309867ab275801f41af117163c7994
Paul Brown <paul.brown@guardian.co.uk>
2017-09-05 19:12:21 +0100

    Some more perf tweaks and code tidy thanks to ScalaZ

--- membership-attribute-service/app/services/AttributesMaker.scala
+++ membership-attribute-service/app/services/AttributesMaker.scala
@@ -1,12 +1,14 @@
 package services
 
-import com.github.nscala_time.time.OrderingImplicits.LocalDateOrdering
+import com.github.nscala_time.time.OrderingImplicits._
 import com.gu.memsub.subsv2.SubscriptionPlan.AnyPlan
 import com.gu.memsub.subsv2.{GetCurrentPlans, Subscription}
 import com.gu.memsub.{Benefit, Product}
 import com.typesafe.scalalogging.LazyLogging
 import models.Attributes
 import org.joda.time.LocalDate
+
+import scalaz.syntax.std.boolean._
 
 class AttributesMaker extends LazyLogging {
 
@@ -16,10 +18,10 @@ class AttributesMaker extends LazyLogging {
       GetCurrentPlans(subscription, forDate).map(_.list).toList.flatten  // it's expected that users may not have any current plans
     }
     def getTopProduct(subscription: Subscription[AnyPlan]): Option[Product] = {
-      getCurrentPlans(subscription).map(_.product).headOption
+      getCurrentPlans(subscription).headOption.map(_.product)
     }
     def getTopPlanName(subscription: Subscription[AnyPlan]): Option[String] = {
-      getCurrentPlans(subscription).map(_.name).headOption
+      getCurrentPlans(subscription).headOption.map(_.name)
     }
     def getAllBenefits(subscription: Subscription[AnyPlan]): Set[Benefit] = {
       getCurrentPlans(subscription).flatMap(_.charges.benefits.list).toSet
@@ -31,22 +33,20 @@ class AttributesMaker extends LazyLogging {
     val contributionSub = groupedSubs.getOrElse(Some(Product.Contribution), Nil).headOption     // Assumes only 1 regular contribution per Identity customer
     val subsWhichIncludeDigitalPack = subs.filter(getAllBenefits(_).contains(Benefit.Digipack))
 
-    if (membershipSub.nonEmpty || contributionSub.nonEmpty || subsWhichIncludeDigitalPack.nonEmpty) {
+    val hasAttributableProduct = membershipSub.nonEmpty || contributionSub.nonEmpty || subsWhichIncludeDigitalPack.nonEmpty
+
+    hasAttributableProduct.option {
       val tier: Option[String] = membershipSub.flatMap(getCurrentPlans(_).headOption.map(_.charges.benefits.head.id))
       val recurringContributionPaymentPlan: Option[String] = contributionSub.flatMap(getTopPlanName)
       val membershipJoinDate: Option[LocalDate] = membershipSub.map(_.startDate)
-      val latestDigitalPackExpiryDate: Option[LocalDate] = subsWhichIncludeDigitalPack.map(_.termEndDate).sorted.reverse.headOption
-
-      Some(Attributes(
+      val latestDigitalPackExpiryDate: Option[LocalDate] = Some(subsWhichIncludeDigitalPack.map(_.termEndDate)).filter(_.nonEmpty).map(_.max)
+      Attributes(
         UserId = identityId,
         Tier = tier,
         RecurringContributionPaymentPlan = recurringContributionPaymentPlan,
         MembershipJoinDate = membershipJoinDate,
         DigitalSubscriptionExpiryDate = latestDigitalPackExpiryDate
       )
-      )
-    } else {
-      None
     }
   }
 
