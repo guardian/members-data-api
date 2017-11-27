77a366decea9c310e9fd0fd9928e65548ff1adeb
Paul Brown <paul.brown@guardian.co.uk>
2017-11-27 18:27:18 +0000

    Remove Zuora fully from the healthcheck. Will a bit of extra logging in membership-common's com.gu.zuora.soap.Client scheduled task.

--- membership-attribute-service/app/controllers/HealthCheckController.scala
+++ membership-attribute-service/app/controllers/HealthCheckController.scala
@@ -3,9 +3,7 @@ package controllers
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Results}
 import components.NormalTouchpointComponents._
-import com.github.nscala_time.time.Imports._
-import com.typesafe.scalalogging.LazyLogging
-import services.HealthCheckableService
+import com.typesafe.scalalogging.StrictLogging
 
 trait Test {
   def ok: Boolean
@@ -17,23 +15,10 @@ class BoolTest(name: String, exec: () => Boolean) extends Test {
   override def ok = exec()
 }
 
-class HealthCheckController extends Results with LazyLogging {
+class HealthCheckController extends Results with StrictLogging {
 
-  private lazy val wrappedZuoraService = new HealthCheckableService {
-    private val duration = 2.minutes
-    override def serviceName = "ZuoraService"
-    override def checkHealth: Boolean =
-      if (zuoraService.lastPingTimeWithin(duration)) {
-        true
-      } else {
-        logger.error(s"${this.serviceName} has not responded within the last ${duration.getMinutes} minutes.")
-        // Return true rather than false, because we don't want Zuora to fail the healthcheck.
-        // However we do want to log outages because we have an SLA from Zuora where we can claim compensation.
-        true
-      }
-  }
-
-  private lazy val services = Set(wrappedZuoraService, salesforceService, featureToggleService, attrService, behaviourService)
+  // behaviourService, Stripe and all Zuora services are not critical
+  private lazy val services = Set(salesforceService, featureToggleService, attrService)
 
   private lazy val tests = services.map(service => new BoolTest(service.serviceName, () => service.checkHealth))
 
@@ -44,7 +29,7 @@ class HealthCheckController extends Results with LazyLogging {
       if (failures.isEmpty) {
         Ok(Json.obj("status" -> "ok", "gitCommitId" -> app.BuildInfo.gitCommitId))
       } else {
-        failures.flatMap(_.messages).foreach(msg => logger.error(msg))
+        failures.flatMap(_.messages).foreach(msg => logger.warn(msg))
         ServiceUnavailable("Service Unavailable")
       }
     }
