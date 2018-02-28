c32c787538dd0f8c2cc7791e0a48c8b25c4a749d
pvighi <patricio.vighi@guardian.co.uk>
2018-02-28 16:41:10 +0000

    removed debug output

--- membership-attribute-service/app/monitoring/SentryLogging.scala
+++ membership-attribute-service/app/monitoring/SentryLogging.scala
@@ -3,11 +3,9 @@ package monitoring
 import io.sentry.Sentry
 import configuration.Config
 import scala.collection.JavaConversions._
-import scala.util.{Failure, Success, Try}
 
 object SentryLogging {
   def init() {
-    println(Config.sentryDsn)
     Config.sentryDsn match {
       case None => play.api.Logger.warn("No Sentry logging configured (OK for dev)")
       case Some(sentryDSN) =>
