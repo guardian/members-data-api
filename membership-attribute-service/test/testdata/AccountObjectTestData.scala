962aeab6e43855fd1f17ead12969f56eaf86dd10
Paul Brown <paul.brown@guardian.co.uk>
2018-03-02 14:27:13 +0000

    Fix broken unit test

--- membership-attribute-service/test/testdata/AccountObjectTestData.scala
+++ membership-attribute-service/test/testdata/AccountObjectTestData.scala
@@ -2,6 +2,7 @@ package testdata
 
 import com.gu.i18n.Currency.GBP
 import com.gu.memsub.Subscription.AccountId
+import com.gu.zuora.api.StripeUKMembershipGateway
 import com.gu.zuora.rest.ZuoraRestService.{AccountObject, PaymentMethodId}
 
 object AccountObjectTestData {
@@ -9,6 +10,6 @@ object AccountObjectTestData {
   private val testPaymentMethodId = PaymentMethodId("testme")
   private val testIdentityId = "123"
   private val currency = GBP
-  val accountObjectWithBalance = AccountObject(testAccountId, 20.0, Some(currency), Some(testPaymentMethodId))
+  val accountObjectWithBalance = AccountObject(testAccountId, 20.0, Some(currency), Some(testPaymentMethodId), Some(StripeUKMembershipGateway))
   val accountObjectWithZeroBalance = AccountObject(testAccountId, 0, Some(currency), Some(testPaymentMethodId))
 }
