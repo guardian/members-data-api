ff061e1a316ceea00a06c918349e1d2b34aa8509
Leigh-Anne Mathieson <leigh-anne.mathieson@guardian.co.uk>
2018-05-29 14:24:10 +0100

    Use SafeLogger

--- membership-attribute-service/app/monitoring/SentryLogging.scala
+++ membership-attribute-service/app/monitoring/SentryLogging.scala
@@ -1,23 +1,26 @@
 package monitoring
 
+import com.gu.monitoring.SafeLogger
+import com.gu.monitoring.SafeLogger._
 import io.sentry.Sentry
 import configuration.Config
+
 import scala.collection.JavaConversions._
 import scala.util.{Failure, Try}
 
 object SentryLogging {
   def init() {
       Config.sentryDsn match {
-        case None => play.api.Logger.warn("No Sentry logging configured (OK for dev)")
+        case None => SafeLogger.warn("No Sentry logging configured (OK for dev)")
         case Some(sentryDSN) =>
-          play.api.Logger.info(s"Initialising Sentry logging")
+          SafeLogger.info(s"Initialising Sentry logging")
           Try {
             val sentryClient = Sentry.init(sentryDSN)
             val buildInfo: Map[String, String] = app.BuildInfo.toMap.mapValues(_.toString)
             val tags = Map("stage" -> Config.stage) ++ buildInfo
             sentryClient.setTags(tags)
           } match {
-            case Failure(e) => play.api.Logger.warn(s"Something went wrong when setting up Sentry logging ${e.getStackTrace}")
+            case Failure(e) => SafeLogger.error(scrub"Something went wrong when setting up Sentry logging ${e.getStackTrace}")
           }
       }
 
