62f1dcdb07a7ef5081e8a3ffd42c9fd0770eed84
Tom Richards <thomas.w.richards@gmail.com>
2019-03-19 18:21:44 +0000

    existing-payment-options: now filters out expired cards/mandates

--- membership-attribute-service/app/controllers/ExistingPaymentOptionsController.scala
+++ membership-attribute-service/app/controllers/ExistingPaymentOptionsController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import actions._
-import com.gu.memsub.{GoCardless, PaymentCard}
+import com.gu.memsub.{GoCardless, PaymentCard, PaymentCardDetails, PaymentMethod}
 import com.gu.memsub.Subscription.AccountId
 import com.gu.memsub.subsv2.reads.ChargeListReads._
 import com.gu.memsub.subsv2.reads.SubPlanReads._
@@ -10,9 +10,9 @@ import com.gu.memsub.subsv2.{Subscription, SubscriptionPlan}
 import com.gu.salesforce.SimpleContactRepository
 import com.typesafe.scalalogging.LazyLogging
 import models.ExistingPaymentOption
-import org.joda.time.LocalDate
+import org.joda.time.{LocalDate}
 import org.joda.time.LocalDate.now
-import play.api.libs.json.{JsNull, Json}
+import play.api.libs.json.{Json}
 import play.api.mvc.{BaseController, ControllerComponents}
 import scalaz.std.option._
 import scalaz.std.scalaFuture._
@@ -44,11 +44,28 @@ class ExistingPaymentOptionsController(commonActions: CommonActions, override va
     subscriptions <- OptionEither.liftEitherOption(subService.since[SubscriptionPlan.AnyPlan](date)(contact))
   } yield subscriptions.groupBy(_.accountId).toList
 
+  def cardThatWontBeExpiredOnFirstTransaction(cardDetails: PaymentCardDetails) =
+    new LocalDate(cardDetails.expiryYear, cardDetails.expiryMonth, 1).isAfter(now.plusMonths(1))
+
   def existingPaymentOptions(cardEnabled: Boolean, directDebitEnabled: Boolean, currencyFilter: String) = BackendFromCookieAction.async { implicit request =>
     implicit val tp = request.touchpoint
     val maybeUserId = authenticationService.userId
 
     val eligibilityDate = now.minusMonths(3)
+
+    val defaultMandateIdIfApplicable = "CLEARED"
+
+    def isPaymentMethodMatchingFilters(paymentMethodOption: Option[PaymentMethod]) = paymentMethodOption match {
+      case Some(_: PaymentCard) => cardEnabled
+      case Some(_: GoCardless) => directDebitEnabled
+      case _ => false
+    }
+
+    def isPaymentMethodStillValid(paymentMethodOption: Option[PaymentMethod]) = paymentMethodOption match {
+      case Some(card: PaymentCard) => card.paymentCardDetails.exists(cardThatWontBeExpiredOnFirstTransaction)
+      case Some(dd: GoCardless) => dd.mandateId != defaultMandateIdIfApplicable //i.e. mandateId a real reference and hasn't been cleared in Zuora because of mandate failure
+      case _ => false
+    }
 
     logger.info(s"Attempting to retrieve existing payment options for identity user: ${maybeUserId.mkString}")
     (for {
@@ -56,13 +73,11 @@ class ExistingPaymentOptionsController(commonActions: CommonActions, override va
       groupedSubsList <- ListEither.fromOptionEither(allSubscriptionsSince(eligibilityDate)(tp.contactRepo, tp.subService)(maybeUserId))
       (accountId, subscriptions) = groupedSubsList
       accountSummary <- ListEither.liftList(tp.zuoraRestService.getAccount(accountId).recover { case x => \/.left(s"error receiving account summary for with account id $accountId. Reason: $x") })
-      if accountSummary.currency.map(_.iso).contains(currencyFilter)
-      paymentMethodOption <- ListEither.liftList(tp.paymentService.getPaymentMethod(accountId).map(\/.right).recover { case x => \/.left(s"error retrieving payment method for account: $accountId. Reason: $x") })
-      if (paymentMethodOption match {
-        case Some(_: PaymentCard) => cardEnabled
-        case Some(_: GoCardless) => directDebitEnabled
-        case _ => false
-      })
+      if accountSummary.currency.map(_.iso).contains(currencyFilter) &&
+         accountSummary.defaultPaymentMethod.isDefined
+      paymentMethodOption <- ListEither.liftList(tp.paymentService.getPaymentMethod(accountId, Some(defaultMandateIdIfApplicable)).map(\/.right).recover { case x => \/.left(s"error retrieving payment method for account: $accountId. Reason: $x") })
+      if isPaymentMethodMatchingFilters(paymentMethodOption) &&
+         isPaymentMethodStillValid(paymentMethodOption)
     } yield ExistingPaymentOption(isFreshlySignedIn, accountSummary, paymentMethodOption, subscriptions).toJson).run.run.map {
       case \/-(jsonList) =>
         logger.info(s"Successfully retrieved eligible existing payment options for identity user: ${maybeUserId.mkString}")
