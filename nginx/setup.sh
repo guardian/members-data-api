844250fb5b725e0f65111172fbdc4e6d94d09033
Francis Rhys-Jones <francis@rhys-jones.com>
2019-10-07 10:26:24 +0100

    adding cleanup of dev config file from pre dev-nginx location

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# Setup Nginx proxies for local development with valid SSL
+#Clean up legacy config
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+NGINX_HOME=$(nginx -V 2>&1 | grep 'configure arguments:' | sed 's#.*conf-path=\([^ ]*\)/nginx\.conf.*#\1#g')
+sudo rm -f $NGINX_HOME/sites-enabled/members-data-api.conf
 
+# Setup Nginx proxies for local development with valid SSL
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 SITE_CONF=${SCRIPT_DIR}/members-data-api.conf
