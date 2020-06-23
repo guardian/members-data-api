c145ce7beb0da8d4d5492a1e0080e7dedbef811e
Mario Galic <mario.galic@guardian.co.uk>
2020-06-23 19:30:19 +0100

    ConcurrentHashMap[String, AtomicInteger] instead of ConcurrentHashMap[String, Int]

--- membership-attribute-service/app/monitoring/Metrics.scala
+++ membership-attribute-service/app/monitoring/Metrics.scala
@@ -1,6 +1,7 @@
 package monitoring
 
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
 
 import akka.actor.ActorSystem
 import com.amazonaws.services.cloudwatch.model.{MetricDatum, PutMetricDataRequest, StatisticSet}
@@ -17,13 +18,44 @@ case class Metrics(service: String) extends CloudWatch {
   val application = Config.applicationName // This sets the namespace for Custom Metrics in AWS (see CloudWatch)
 }
 
+//// Designed for high-frequency metrics, for example, 1000 per minute is about $450 per month
+//final class ExpensiveMetrics(
+//  val service: String
+//)(implicit system: ActorSystem, ec: ExecutionContext) extends CloudWatch with LazyLogging {
+//  logger.info("expensive - created instance")
+//
+//  import scala.collection.JavaConverters._
+//  private val chm = new ConcurrentHashMap[String, Int]().asScala
+//
+//  val stage = Config.stage
+//  val application = Config.applicationName
+//
+//  system.scheduler.schedule(5.seconds, 60.seconds)(publishAllMetrics())
+//
+//  def countRequest(key: String): Unit = {
+//    chm.get(key) match {
+//      case Some(value) => chm.update(key, value + 1)
+//      case None => chm.update(key, 1)
+//    }
+//  }
+//
+//  private def resetCount(key: String) = chm.replace(key, 0)
+//
+//  private def publishAllMetrics(): Unit =
+//    chm foreach { case (key, value) =>
+//      put(key, value)
+//      resetCount(key)
+//    }
+//}
+
 // Designed for high-frequency metrics, for example, 1000 per minute is about $450 per month
 final class ExpensiveMetrics(
   val service: String
 )(implicit system: ActorSystem, ec: ExecutionContext) extends CloudWatch with LazyLogging {
   logger.info("expensive - created instance")
+
   import scala.collection.JavaConverters._
-  private val chm = new ConcurrentHashMap[String, Int]().asScala
+  private val chm = new ConcurrentHashMap[String, AtomicInteger]().asScala
 
   val stage = Config.stage
   val application = Config.applicationName
@@ -31,44 +63,14 @@ final class ExpensiveMetrics(
   system.scheduler.schedule(5.seconds, 60.seconds)(publishAllMetrics())
 
   def countRequest(key: String): Unit = {
-    chm.get(key) match {
-      case Some(value) => chm.update(key, value + 1)
-      case None => chm.update(key, 1)
-    }
+    chm.getOrElseUpdate(key, new AtomicInteger(1)).incrementAndGet()
   }
-
-  private def resetCount(key: String) = chm.replace(key, 0)
+  private def resetCount(key: String): Unit =
+    chm.getOrElseUpdate(key, new AtomicInteger(0)).set(0)
 
   private def publishAllMetrics(): Unit =
     chm foreach { case (key, value) =>
-      putStatisticsSetCount(key, value)
-      val result = resetCount(key)
-      logger.info(s"expensive reset result $result")
+      put(key, value.doubleValue())
+      resetCount(key)
     }
-
-//  private def putStatisticsSetCount(name: String, count: Int): Unit = {
-//    logger.info(s"expensive metric: $name=$count")
-//    val stats = new StatisticSet()
-//      .withMaximum(1d)
-//      .withMinimum(1d)
-//      .withSampleCount(count.toDouble)
-//      .withSum(count.toDouble)
-//
-//    val metric = new MetricDatum()
-//      .withMetricName(name)
-//      .withUnit("Count")
-//      .withDimensions(mandatoryDimensions:_*)
-//      .withStatisticValues(stats)
-//
-//    val request = new PutMetricDataRequest().
-//      withNamespace(application)
-//      .withMetricData(metric)
-//
-//    cloudwatch.putMetricDataAsync(request, LoggingAsyncHandler)
-//  }
-
-  private def putStatisticsSetCount(name: String, count: Int) = {
-    put(name, count)
-  }
-
 }
