dc20b5fc90cdf442dc699df755594e868dab4a40
Kelvin Chappell <kelvin.chappell@theguardian.com>
2020-10-20 11:58:54 +0100

    Bump membership-common to v0.589

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,8 +7,8 @@ object Dependencies {
 
   val sentryLogback = "io.sentry" % "sentry-logback" % "1.7.5"
   val identityAuth = "com.gu.identity" %% "identity-auth-play" % "3.195"
-  val identityTestUsers =  "com.gu" %% "identity-test-users" % "0.7"
-  val postgres =  "org.postgresql" % "postgresql" % "42.2.1"
+  val identityTestUsers = "com.gu" %% "identity-test-users" % "0.7"
+  val postgres = "org.postgresql" % "postgresql" % "42.2.1"
   val jdbc = PlayImport.jdbc
   val playWS = PlayImport.ws
   val playFilters = PlayImport.filters
@@ -17,7 +17,7 @@ object Dependencies {
   val awsDynamo = "com.amazonaws" % "aws-java-sdk-dynamodb" % awsClientVersion
   val awsSQS = "com.amazonaws" % "aws-java-sdk-sqs" % awsClientVersion
   val awsCloudWatch = "com.amazonaws" % "aws-java-sdk-cloudwatch" % awsClientVersion
-  val membershipCommon = "com.gu" %% "membership-common" % "0.588"
+  val membershipCommon = "com.gu" %% "membership-common" % "0.589"
   val scalaz = "org.scalaz" %% "scalaz-core" % "7.2.9"
   val kinesis = "com.gu" % "kinesis-logback-appender" % "1.4.2"
   val logstash = "net.logstash.logback" % "logstash-logback-encoder" % "4.9"
@@ -25,9 +25,24 @@ object Dependencies {
 
   //projects
 
-  val apiDependencies = Seq(jdbc, postgres, sentryLogback, identityAuth, identityTestUsers,
-    playWS, playFilters, scanamo, awsDynamo, awsSQS, awsCloudWatch, scalaz, membershipCommon,
-    specs2, kinesis, logstash, anorm,
+  val apiDependencies = Seq(
+    jdbc,
+    postgres,
+    sentryLogback,
+    identityAuth,
+    identityTestUsers,
+    playWS,
+    playFilters,
+    scanamo,
+    awsDynamo,
+    awsSQS,
+    awsCloudWatch,
+    scalaz,
+    membershipCommon,
+    specs2,
+    kinesis,
+    logstash,
+    anorm,
     "com.amazonaws" % "aws-java-sdk-autoscaling" % awsClientVersion
   )
 
