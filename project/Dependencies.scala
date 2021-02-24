4ff10b512037e23fc5969ce4971280b5e16c9ae5
Kelvin Chappell <kelvin.chappell@theguardian.com>
2021-02-24 13:43:08 +0000

    Update kinesis-logback-appender to 1.4.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val awsCloudWatch = "com.amazonaws" % "aws-java-sdk-cloudwatch" % awsClientVersion
   val membershipCommon = "com.gu" %% "membership-common" % "0.592"
   val scalaz = "org.scalaz" %% "scalaz-core" % "7.2.9"
-  val kinesis = "com.gu" % "kinesis-logback-appender" % "1.4.2"
+  val kinesis = "com.gu" % "kinesis-logback-appender" % "1.4.4"
   val logstash = "net.logstash.logback" % "logstash-logback-encoder" % "4.9"
   val anorm = "org.playframework.anorm" %% "anorm" % "2.6.0"
 
