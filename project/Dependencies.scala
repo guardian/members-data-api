bad445a7c6c934c7011bfc4463696f55eca94d01
Kelvin Chappell <kelvin.chappell@theguardian.com>
2021-02-24 13:44:16 +0000

    Update postgresql to 42.2.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val sentryLogback = "io.sentry" % "sentry-logback" % "1.7.5"
   val identityAuth = "com.gu.identity" %% "identity-auth-play" % "3.235"
   val identityTestUsers = "com.gu" %% "identity-test-users" % "0.7"
-  val postgres = "org.postgresql" % "postgresql" % "42.2.18"
+  val postgres = "org.postgresql" % "postgresql" % "42.2.19"
   val jdbc = PlayImport.jdbc
   val playWS = PlayImport.ws
   val playFilters = PlayImport.filters
