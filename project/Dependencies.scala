f2ddf2d7d5b8b76a7aaa59da4ca8b0955051174c
Scala Steward <me@scala-steward.org>
2021-11-16 10:46:50 +0100

    Update guava to 31.0.1-jre

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -55,7 +55,7 @@ object Dependencies {
     "com.amazonaws" % "aws-java-sdk-autoscaling" % awsClientVersion,
     netty,
     nettyHttp,
-    "com.google.guava" % "guava" % "30.1.1-jre" // until https://github.com/playframework/playframework/pull/10874
+    "com.google.guava" % "guava" % "31.0.1-jre" // until https://github.com/playframework/playframework/pull/10874
   ) ++ jackson
 
   val depOverrides = jackson
