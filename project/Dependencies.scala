6705714dc1c78a330e2d880b323a7e8da8e18bcd
Scala Steward <me@scala-steward.org>
2022-01-20 12:16:40 +0100

    Update scalaz-core to 7.3.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val awsSQS = "com.amazonaws" % "aws-java-sdk-sqs" % awsClientVersion
   val awsCloudWatch = "com.amazonaws" % "aws-java-sdk-cloudwatch" % awsClientVersion
   val membershipCommon = "com.gu" %% "membership-common" % "0.610"
-  val scalaz = "org.scalaz" %% "scalaz-core" % "7.2.34"
+  val scalaz = "org.scalaz" %% "scalaz-core" % "7.3.6"
   val kinesis = "com.gu" % "kinesis-logback-appender" % "2.0.3"
   val logstash = "net.logstash.logback" % "logstash-logback-encoder" % "4.9"
   val anorm = "org.playframework.anorm" %% "anorm" % "2.6.10"
