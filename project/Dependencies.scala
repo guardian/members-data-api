e9770318ed88ce33644b6d68332d2aad243cb54d
Scala Steward <scala-steward@users.noreply.github.com>
2022-06-10 13:20:54 +0000

    Update identity-auth-play to 3.255

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val awsClientV2Version = "2.16.86"
 
   val sentryLogback = "io.sentry" % "sentry-logback" % "1.7.5"
-  val identityAuth = "com.gu.identity" %% "identity-auth-play" % "3.254"
+  val identityAuth = "com.gu.identity" %% "identity-auth-play" % "3.255"
   val identityTestUsers = "com.gu" %% "identity-test-users" % "0.8"
   val postgres = "org.postgresql" % "postgresql" % "42.3.3"
   val jdbc = PlayImport.jdbc
