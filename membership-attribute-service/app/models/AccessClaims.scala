22f48eaf53852635336db82bebbfd462c2930d4e
Kelvin Chappell <kelvin.chappell@theguardian.com>
2022-10-14 16:43:38 +0100

    Change AccessClaims to comply with UserClaims

--- membership-attribute-service/app/models/AccessClaims.scala
+++ membership-attribute-service/app/models/AccessClaims.scala
@@ -1,6 +1,6 @@
 package models
 
-import com.gu.identity.auth.{ClaimsParser, DefaultClaimsParser, UnparsedClaims, UserClaims, ValidationError}
+import com.gu.identity.auth._
 import com.gu.identity.model.User
 
 /** Claims that are used to determine which resources the user is authorised to access.
@@ -15,27 +15,31 @@ import com.gu.identity.model.User
   *   true iff the user has validated their email address
   */
 case class AccessClaims(
-                         primaryEmailAddress: String,
-                         identityId: String,
-                         username: Option[String] = None,
-                         userEmailValidated: Option[Boolean] = None,
+    primaryEmailAddress: String,
+    identityId: String,
+    username: Option[String] = None,
+    userEmailValidated: Option[Boolean] = None,
 ) extends UserClaims
 
 object AccessClaimsParser extends ClaimsParser[AccessClaims] {
-  override def fromUnparsed(unparsedClaims: UnparsedClaims): Either[ValidationError, AccessClaims] = DefaultClaimsParser.fromUnparsed(unparsedClaims).map(
-    defaultClaims =>
+
+  override def fromDefaultAndUnparsed(
+      defaultClaims: DefaultUserClaims,
+      unparsedClaims: UnparsedClaims,
+  ): Either[ValidationError, AccessClaims] =
+    Right(
       AccessClaims(
         primaryEmailAddress = defaultClaims.primaryEmailAddress,
         identityId = defaultClaims.identityId,
         username = defaultClaims.username,
-        userEmailValidated = unparsedClaims.getOptional[Boolean]("email_validated")
-      )
-  )
+        userEmailValidated = unparsedClaims.getOptional[Boolean]("email_validated"),
+      ),
+    )
 
   override def fromUser(user: User): AccessClaims = AccessClaims(
     primaryEmailAddress = user.primaryEmailAddress,
     identityId = user.id,
     username = user.publicFields.username,
-    userEmailValidated = user.statusFields.userEmailValidated
+    userEmailValidated = user.statusFields.userEmailValidated,
   )
 }
