2bda3c7ad799bc35c267b60e54153cf58614c2b4
Pawel Krupinski <pawel.krupinski.casual@guardian.co.uk>
2022-11-09 11:25:32 +0000

    Upgrade SBT and dependencies.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,35 +1,37 @@
 import sbt._
 import play.sbt.PlayImport
+import sbt.Keys.dependencyOverrides
 
 object Dependencies {
 
-  val awsClientVersion = "1.11.1022"
-  val awsClientV2Version = "2.16.86"
+  val awsClientVersion = "1.12.338"
+  val awsClientV2Version = "2.18.13"
 
   val sentryLogback = "io.sentry" % "sentry-logback" % "1.7.5"
   val identityAuth = "com.gu.identity" %% "identity-auth-play" % "4.4"
   val identityTestUsers = "com.gu" %% "identity-test-users" % "0.8"
-  val postgres = "org.postgresql" % "postgresql" % "42.3.3"
+  val postgres = "org.postgresql" % "postgresql" % "42.5.0"
   val jdbc = PlayImport.jdbc
   val playWS = PlayImport.ws
   val playFilters = PlayImport.filters
   val specs2 = PlayImport.specs2 % "test"
-  val scanamo = "org.scanamo" %% "scanamo" % "1.0.0-M15"
+  val scanamo = "org.scanamo" %% "scanamo" % "1.0.0-M23"
   val awsDynamo = "software.amazon.awssdk" % "dynamodb" % awsClientV2Version
   val awsSQS = "com.amazonaws" % "aws-java-sdk-sqs" % awsClientVersion
   val awsCloudWatch = "com.amazonaws" % "aws-java-sdk-cloudwatch" % awsClientVersion
-  val membershipCommon = "com.gu" %% "membership-common" % "0.622"
-  val scalaz = "org.scalaz" %% "scalaz-core" % "7.2.34"
-  val kinesis = "com.gu" % "kinesis-logback-appender" % "2.0.3"
-  val logstash = "net.logstash.logback" % "logstash-logback-encoder" % "4.9"
-  val anorm = "org.playframework.anorm" %% "anorm" % "2.6.10"
-  val netty = "io.netty" % "netty-codec" % "4.1.74.Final"
-  val nettyHttp = "io.netty" % "netty-codec-http" % "4.1.74.Final"
+  val membershipCommon = "com.gu" %% "membership-common" % "0.631"
+  val scalaz = "org.scalaz" %% "scalaz-core" % "7.3.6"
+  val kinesis = "com.gu" % "kinesis-logback-appender" % "2.1.0"
+  val logstash = "net.logstash.logback" % "logstash-logback-encoder" % "7.2"
+  val anorm = "org.playframework.anorm" %% "anorm" % "2.7.0"
+  val netty = "io.netty" % "netty-codec" % "4.1.84.Final"
+  val nettyHttp = "io.netty" % "netty-codec-http" % "4.1.84.Final"
+  val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.1.0"
 
-  val jacksonVersion = "2.13.2"
-  val jacksonDatabindVersion = "2.13.2.2"
-  val akkaHttpCore = "com.typesafe.akka" %% "akka-http-core" % "10.2.9"
-  val oktaJwtVerifierVersion = "0.5.5"
+  val jacksonVersion = "2.14.0"
+  val jacksonDatabindVersion = "2.14.0"
+  val akkaHttpCore = "com.typesafe.akka" %% "akka-http-core" % "10.4.0"
+  val oktaJwtVerifierVersion = "0.5.7"
   val jackson = Seq(
     "com.fasterxml.jackson.core" % "jackson-core" % jacksonVersion,
     "com.fasterxml.jackson.core" % "jackson-annotations" % jacksonVersion,
@@ -44,6 +46,8 @@ object Dependencies {
     "com.okta.jwt" % "okta-jwt-verifier" % oktaJwtVerifierVersion,
     "com.okta.jwt" % "okta-jwt-verifier-impl" % oktaJwtVerifierVersion,
   )
+  val awsJavaSdkAutoscaling = "com.amazonaws" % "aws-java-sdk-autoscaling" % awsClientVersion
+
 
   // projects
 
@@ -65,13 +69,12 @@ object Dependencies {
     kinesis,
     logstash,
     anorm,
-    "com.amazonaws" % "aws-java-sdk-autoscaling" % awsClientVersion,
+    awsJavaSdkAutoscaling,
     netty,
     nettyHttp,
-    "com.google.guava" % "guava" % "30.1.1-jre", // until https://github.com/playframework/playframework/pull/10874
     akkaHttpCore,
   ) ++ jackson ++ oktaJwtVerifier
 
-  val depOverrides = jackson
+  val depOverrides = Seq(scalaXml)
 
 }
