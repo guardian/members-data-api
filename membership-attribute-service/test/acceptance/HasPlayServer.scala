4b12226952881c1b3fc968f7ee18ad743194f301
Pawel Krupinski <pawel.krupinski.casual@guardian.co.uk>
2023-01-04 19:24:22 +0000

    Randomizing ports.

--- membership-attribute-service/test/acceptance/HasPlayServer.scala
+++ membership-attribute-service/test/acceptance/HasPlayServer.scala
@@ -1,5 +1,6 @@
 package acceptance
 
+import acceptance.util.AvailablePort
 import com.typesafe.config.ConfigFactory
 import play.api.ApplicationLoader.Context
 import play.api.inject.ApplicationLifecycle
@@ -13,7 +14,7 @@ import scala.concurrent.Future
 trait HasPlayServer {
   this: HasIdentityMockServer =>
 
-  private val playPort = 8081
+  private val playPort = AvailablePort.find()
   private val playServerAddress = "http://localhost:" + playPort
   protected def endpointUrl(path: String) = playServerAddress + path
 
