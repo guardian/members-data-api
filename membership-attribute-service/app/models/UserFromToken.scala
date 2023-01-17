5880662787d6421af02aae002021222a3bc73eb2
Kelvin Chappell <kelvin.chappell@theguardian.com>
2023-01-17 11:59:30 +0000

    Revert "Add authTime to access claims (#888)" (#891)
    This reverts commit 769208a621756bd7dbe7d085c54a80cc7e5baecf.

--- membership-attribute-service/app/models/UserFromToken.scala
+++ membership-attribute-service/app/models/UserFromToken.scala
@@ -3,8 +3,6 @@ package models
 import com.gu.identity.auth._
 import com.gu.identity.model.User
 
-import java.time.{Instant, ZoneId, ZonedDateTime}
-
 /** Claims that are used to determine which resources the user is authorised to access.
   *
   * @param primaryEmailAddress
@@ -15,15 +13,12 @@ import java.time.{Instant, ZoneId, ZonedDateTime}
   *   User name, a synonym for display name. Also used to determine if this is a test user.
   * @param userEmailValidated
   *   true iff the user has validated their email address
-  * @param authTime
-  *   The time the user was last authenticated.
   */
 case class UserFromToken(
     primaryEmailAddress: String,
     identityId: String,
     username: Option[String] = None,
     userEmailValidated: Option[Boolean] = None,
-    authTime: Option[ZonedDateTime], // optional because not available from Idapi
 ) extends AccessClaims
 
 object UserFromTokenParser extends AccessClaimsParser[UserFromToken] {
@@ -31,24 +26,20 @@ object UserFromTokenParser extends AccessClaimsParser[UserFromToken] {
   override def fromDefaultAndUnparsed(
       defaultClaims: DefaultAccessClaims,
       unparsedClaims: UnparsedClaims,
-  ): Either[ValidationError, UserFromToken] = {
-    def toUtcTime(unixTimeStamp: Long) = Instant.ofEpochSecond(unixTimeStamp).atZone(ZoneId.of("UTC"))
-    for {
-      authTime <- unparsedClaims.getRequired[Long]("auth_time")
-    } yield UserFromToken(
-      primaryEmailAddress = defaultClaims.primaryEmailAddress,
-      identityId = defaultClaims.identityId,
-      username = defaultClaims.username,
-      userEmailValidated = unparsedClaims.getOptional[Boolean]("email_validated"),
-      authTime = Some(toUtcTime(authTime)),
+  ): Either[ValidationError, UserFromToken] =
+    Right(
+      UserFromToken(
+        primaryEmailAddress = defaultClaims.primaryEmailAddress,
+        identityId = defaultClaims.identityId,
+        username = defaultClaims.username,
+        userEmailValidated = unparsedClaims.getOptional[Boolean]("email_validated"),
+      ),
     )
-  }
 
   override def fromUser(user: User): UserFromToken = UserFromToken(
     primaryEmailAddress = user.primaryEmailAddress,
     identityId = user.id,
     username = user.publicFields.username,
     userEmailValidated = user.statusFields.userEmailValidated,
-    authTime = None,
   )
 }
