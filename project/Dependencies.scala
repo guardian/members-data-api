ad3b499ccee306a5c53b8a96a6d489b9f5ab92ef
Patricio Vighi <pvighi@gmail.com>
2023-02-07 10:10:37 +0000

    test updated lib

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val awsClientV2Version = "2.16.104"
 
   val sentryLogback = "io.sentry" % "sentry-logback" % "1.7.5"
-  val identityAuth = "com.gu.identity" %% "identity-auth-play" % "4.9"
+  val identityAuth = "com.gu.identity" %% "identity-auth-play" % "4.10-SNAPSHOT"
   val identityTestUsers = "com.gu" %% "identity-test-users" % "0.8"
   val postgres = "org.postgresql" % "postgresql" % "42.5.1"
   val jdbc = PlayImport.jdbc
