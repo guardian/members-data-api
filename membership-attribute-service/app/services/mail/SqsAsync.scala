9c11d0fd2202d3db9194a3f868903bc637b50605
Pawel Krupinski <pawel.krupinski.casual@guardian.co.uk>
2023-03-22 22:03:22 +0000

    Refactoring SQSAsync.

--- membership-attribute-service/app/services/mail/SqsAsync.scala
+++ membership-attribute-service/app/services/mail/SqsAsync.scala
@@ -1,9 +1,11 @@
 package services.mail
 
 import com.typesafe.scalalogging.LazyLogging
+import services.mail.SqsAsync.CredentialsProvider
+import software.amazon.awssdk.auth.credentials.{AwsCredentialsProviderChain, InstanceProfileCredentialsProvider, ProfileCredentialsProvider}
 import software.amazon.awssdk.regions.Region.EU_WEST_1
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
-import software.amazon.awssdk.services.sqs.model.{GetQueueUrlRequest, SendMessageRequest}
+import software.amazon.awssdk.services.sqs.model.{GetQueueUrlRequest, SendMessageRequest, SendMessageResponse}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -16,31 +18,45 @@ class SqsAsync extends LazyLogging {
 
   val client = SqsAsyncClient.builder
     .region(EU_WEST_1)
-    .credentialsProvider(AwsSQSSend.CredentialsProvider)
+    .credentialsProvider(CredentialsProvider)
     .build()
 
   def send(queueName: QueueName, payload: String): Future[Unit] = {
-    val futureQueueUrl =
-      client
-        .getQueueUrl(
-          GetQueueUrlRequest.builder.queueName(queueName.value).build(),
-        )
-        .asScala
-        .map(_.queueUrl)
-
     for {
-      queueUrl <- futureQueueUrl
-      _ <- Future.successful(logger.info(s"Sending message to SQS queue $queueUrl"))
-      request = SendMessageRequest.builder.queueUrl(queueUrl).messageBody(payload).build()
-      response = client.sendMessage(request).asScala
-      _ <- response.transform {
+      queueUrl <- queueUrlFor(queueName)
+      _ <- sendToUrl(queueUrl, payload).transform {
         case Success(result) =>
           logger.info(s"Successfully sent message to $queueUrl: $result")
           Success(())
         case Failure(throwable) =>
-          logger.error(s"Failed to send message to $queueUrl due to:", throwable)
+          logger.error(s"Failed to sendToUrl message to $queueUrl due to:", throwable)
           Failure(throwable)
       }
     } yield ()
   }
+
+  private def queueUrlFor(queueName: QueueName): Future[String] =
+    client
+      .getQueueUrl(
+        GetQueueUrlRequest.builder.queueName(queueName.value).build(),
+      )
+      .asScala
+      .map(_.queueUrl)
+
+  private def sendToUrl(queueUrl: String, payload: String): Future[SendMessageResponse] = {
+    val request = SendMessageRequest.builder.queueUrl(queueUrl).messageBody(payload).build()
+    logger.info(s"Sending message to SQS queue $queueUrl")
+    client.sendMessage(request).asScala
+  }
+}
+
+object SqsAsync {
+  val ProfileName = "membership"
+
+  lazy val CredentialsProvider: AwsCredentialsProviderChain = AwsCredentialsProviderChain.builder
+    .credentialsProviders(
+      ProfileCredentialsProvider.create(ProfileName),
+      InstanceProfileCredentialsProvider.create(),
+    )
+    .build()
 }
