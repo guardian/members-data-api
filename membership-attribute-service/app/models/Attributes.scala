d26f15c2a8a06dc1dd167f1eb6e74c9ed511bbc9
Paul Brown <paulbrown1982@yahoo.co.uk>
2024-02-09 16:51:54 +0000

    Forgot to add staff

--- membership-attribute-service/app/models/Attributes.scala
+++ membership-attribute-service/app/models/Attributes.scala
@@ -74,7 +74,7 @@ case class Attributes(
     paperSubscriber = isPaperSubscriber,
     guardianWeeklySubscriber = isGuardianWeeklySubscriber,
     guardianPatron = isGuardianPatron,
-    feast = isGuardianPatron || isPartnerTier || isPatronTier || isGuardianPatron ||
+    feast = isStaffTier || isPatronTier || isPartnerTier || isPatronTier || isGuardianPatron ||
       (isSupporterPlus && SupporterPlusAcquisitionDate.exists(isBeforeFeastLaunch))
   )
 
