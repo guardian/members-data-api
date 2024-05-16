3db075896129fa2cc5a51c1eec09848d53eb0b7b
John Duffell <john.duffell@guardian.co.uk>
2024-05-16 11:09:59 +0100

    make supporter plus a subscription in MMA rather than recurring support

--- membership-attribute-service/app/models/AccountDetails.scala
+++ membership-attribute-service/app/models/AccountDetails.scala
@@ -221,7 +221,7 @@ object AccountDetails {
   def mmaCategoryFrom(product: Product): String = product match {
     case _: Product.Paper => "subscriptions" // Paper includes GW 🤦‍
     case _: Product.ZDigipack => "subscriptions"
-    case _: Product.SupporterPlus => "recurringSupport"
+    case _: Product.SupporterPlus => "subscriptions"
     case _: Product.GuardianPatron => "subscriptions"
     case _: Product.Contribution => "recurringSupport"
     case _: Product.Membership => "membership"
