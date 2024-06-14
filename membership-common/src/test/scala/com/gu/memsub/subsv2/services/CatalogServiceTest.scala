1c6b06061bd5de7fae998004c8c5361f147fd2c8
Rupert Bates <Rupert.Bates@guardian.co.uk>
2024-06-14 12:27:16 +0100

    Fix test

--- membership-common/src/test/scala/com/gu/memsub/subsv2/services/CatalogServiceTest.scala
+++ membership-common/src/test/scala/com/gu/memsub/subsv2/services/CatalogServiceTest.scala
@@ -25,8 +25,8 @@ class CatalogServiceTest extends Specification {
       val cats = CatalogService.read(FetchCatalog.fromZuoraApi(CatalogServiceTest.client("rest/Catalog.json")))
       val supporterPlus = cats(ProductRatePlanId("8ad08e1a8586721801858805663f6fab"))
       val supporterPlusMonth = cats(ProductRatePlanId("8ad08cbd8586721c01858804e3275376"))
-      supporterPlus.charges.head.billingPeriod must beSome(ZYear)
-      supporterPlusMonth.charges.head.billingPeriod must beSome(ZMonth)
+      supporterPlus.charges.head.billingPeriod must beEqualTo(Some(ZYear))
+      supporterPlusMonth.charges.head.billingPeriod must beEqualTo(Some(ZMonth))
 
       val tierThree = cats.collect { case (_, plan) if plan.productId == ids.tierThree && plan.status == Status.Current => plan }.toList
       tierThree.size must beEqualTo(4)
