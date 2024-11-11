b6e40fbe0c6b0fda41f5419daad1bd3576bf4add
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-11-11 15:23:15 +0000

    build: bump jackson package to remove high vulnerability

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,8 +27,8 @@ object Dependencies {
   val mockitoScala = "org.mockito" %% "mockito-scala" % "1.17.14" % Test
   val logback = "ch.qos.logback" % "logback-classic" % "1.4.14"
 
-  val jacksonVersion = "2.14.2"
-  val jacksonDatabindVersion = "2.14.2"
+  val jacksonVersion = "2.15.0"
+  val jacksonDatabindVersion = "2.15.0"
   val akkaHttpCore = "com.typesafe.akka" %% "akka-http-core" % "10.2.9"
   val oktaJwtVerifierVersion = "0.5.7"
   val jackson = Seq(
