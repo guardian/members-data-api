a8a3892da5cd57804a7d1bf6ead5ee4888f30247
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2025-03-11 11:23:36 +0000

    build: bump io.netty version to remove high vulnerability CVE-2025-24970

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,8 +20,8 @@ object Dependencies {
   val awsCloudWatch = "com.amazonaws" % "aws-java-sdk-cloudwatch" % awsClientVersion
   val scalaz = "org.scalaz" %% "scalaz-core" % "7.3.8"
   val anorm = "org.playframework.anorm" %% "anorm" % "2.7.0"
-  val netty = "io.netty" % "netty-codec" % "4.1.87.Final"
-  val nettyHttp = "io.netty" % "netty-codec-http" % "4.1.87.Final"
+  val netty = "io.netty" % "netty-codec" % "4.1.118.Final"
+  val nettyHttp = "io.netty" % "netty-codec-http" % "4.1.118.Final"
   val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.1.0"
   val mockServer = "org.mock-server" % "mockserver-netty" % "5.14.0" % Test
   val mockitoScala = "org.mockito" %% "mockito-scala" % "1.17.14" % Test
