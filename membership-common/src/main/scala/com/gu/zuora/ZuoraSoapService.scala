0fbaf7a50b04a0e7110a0c9ef94a7351b1147e9a
andrade0 <andradeolivier@gmail.com>
2026-07-13 10:23:05 +0100

    replace SOAP query reads with Zuora REST equivalents (#1261)
    * replace SOAP query reads with Zuora REST equivalents
    
    * use block comments and a zuora exception for the rest query reads
    
    * pr feedback - deterministic card reads, per-endpoint doc links, drop id wrapper

--- membership-common/src/main/scala/com/gu/zuora/ZuoraSoapService.scala
+++ membership-common/src/main/scala/com/gu/zuora/ZuoraSoapService.scala
@@ -17,6 +17,8 @@ import com.gu.zuora.soap.models.Results.{CreateResult, UpdateResult}
 import com.gu.zuora.soap.models.errors._
 import com.gu.zuora.soap.models.{PaymentSummary, Queries => SoapQueries}
 import com.gu.zuora.soap.writers.Command.createPaymentMethodWrites
+import com.gu.zuora.rest.ZuoraQueryReads._
+import play.api.libs.json.Reads
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -37,20 +39,31 @@ trait SoapClient[M[_]] {
   def getAccountIds(contactId: ContactId)(implicit logPrefix: LogPrefix): M[List[AccountId]]
 }
 
-class ZuoraSoapService(soapClient: soap.Client)(implicit ec: ExecutionContext) extends SoapClient[Future] with SafeLogging {
+class ZuoraSoapService(soapClient: soap.Client, restClient: rest.SimpleClient[Future])(implicit ec: ExecutionContext)
+    extends SoapClient[Future]
+    with SafeLogging {
 
   import ZuoraSoapService._
 
+  /* These reads used to go through the SOAP query API; they now hit the equivalent Zuora REST endpoints (object/{type}/{id} and action/query),
+     mapping the responses back to the same case classes so callers are unchanged. getObject fails the Future if the object is missing or the call
+     errors, like the old queryOne; query returns the records (empty if none) and only fails on a REST error, like the old plural query. */
+  private def getObject[A: Reads](url: String)(implicit logPrefix: LogPrefix): Future[A] =
+    restClient.get[A](url).map(_.valueOr(error => throw QueryError(s"Zuora REST get '$url' failed: $error")))
+
+  private def query[A: Reads](zoql: String)(implicit logPrefix: LogPrefix): Future[List[A]] =
+    restClient
+      .post[RestQuery, QueryResponse[A]]("action/query", RestQuery(zoql))
+      .map(_.valueOr(error => throw QueryError(s"Zuora REST query '$zoql' failed: $error")).records)
+
   def getAccountIds(contactId: ContactId)(implicit logPrefix: LogPrefix): Future[List[AccountId]] =
-    soapClient
-      .query[SoapQueries.Account](SimpleFilter("crmId", contactId.salesforceAccountId))
-      .map(_.map(a => AccountId(a.id)).toList)
+    query[AccountId](s"select Id from account where crmId = '${contactId.salesforceAccountId}'")
 
   def getAccount(accountId: AccountId)(implicit logPrefix: LogPrefix): Future[SoapQueries.Account] =
-    soapClient.queryOne[SoapQueries.Account](SimpleFilter("id", accountId.get))
+    getObject[SoapQueries.Account](s"object/account/${accountId.get}")
 
   def getContact(contactId: String)(implicit logPrefix: LogPrefix): Future[SoapQueries.Contact] =
-    soapClient.queryOne[SoapQueries.Contact](SimpleFilter("Id", contactId))
+    getObject[SoapQueries.Contact](s"object/contact/$contactId")
 
   private def setDefaultPaymentMethod(
       accountId: AccountId,
@@ -116,15 +129,23 @@ class ZuoraSoapService(soapClient: soap.Client)(implicit ec: ExecutionContext) e
     } yield result
   }
 
-  def getPaymentSummary(subscriptionNumber: S.SubscriptionNumber, accountCurrency: Currency)(implicit logPrefix: LogPrefix): Future[PaymentSummary] =
+  def getPaymentSummary(subscriptionNumber: S.SubscriptionNumber, accountCurrency: Currency)(implicit
+      logPrefix: LogPrefix,
+  ): Future[PaymentSummary] = {
+    val zoql = List(
+      "select Id, ChargeAmount, TaxAmount, ServiceStartDate, ServiceEndDate, ChargeNumber, ProductName, SubscriptionId",
+      "from invoiceitem",
+      s"where SubscriptionNumber = '${subscriptionNumber.getNumber}'",
+    ).mkString(" ")
     for {
-      invoiceItems <- soapClient.query[SoapQueries.InvoiceItem](SimpleFilter("SubscriptionNumber", subscriptionNumber.getNumber))
+      invoiceItems <- query[SoapQueries.InvoiceItem](zoql)
     } yield {
       val filteredInvoices = latestInvoiceItems(invoiceItems)
       PaymentSummary(filteredInvoices, accountCurrency)
     }
+  }
 
   def getPaymentMethod(id: String)(implicit logPrefix: LogPrefix): Future[SoapQueries.PaymentMethod] =
-    soapClient.queryOne[SoapQueries.PaymentMethod](SimpleFilter("Id", id))
+    getObject[SoapQueries.PaymentMethod](s"object/payment-method/$id")
 
 }
---
+++ membership-common/src/main/scala/com/gu/zuora/rest/ZuoraQueryReads.scala
@@ -0,0 +1,131 @@
+package com.gu.zuora.rest
+
+import com.gu.i18n.Currency
+import com.gu.memsub.Subscription.AccountId
+import com.gu.zuora.ZuoraLookup
+import com.gu.zuora.api.PaymentGateway
+import com.gu.zuora.soap.models.Queries
+import org.joda.time.LocalDate
+import play.api.libs.json._
+
+/** JSON readers that let ZuoraSoapService's read methods hit Zuora REST (object/{type}/{id} and action/query) instead of SOAP query, mapping the
+  * responses back to the existing SOAP query case classes so callers are unaffected. Field names and parsing mirror the old SOAP readers in
+  * com.gu.zuora.soap.Readers. Each reader links the Zuora operation it parses.
+  */
+object ZuoraQueryReads {
+
+  case class RestQuery(queryString: String)
+  implicit val restQueryWrites: Writes[RestQuery] = Json.writes[RestQuery]
+
+  case class QueryResponse[A](records: List[A])
+  implicit def queryResponseReads[A](implicit r: Reads[A]): Reads[QueryResponse[A]] =
+    (__ \ "records").read[List[A]].map(QueryResponse(_))
+
+  /** @see https://developer.zuora.com/api-references/api/operation/Action_POSTquery */
+  implicit val accountIdReads: Reads[AccountId] = (__ \ "Id").read[String].map(AccountId)
+
+  /** @see https://developer.zuora.com/api-references/older-api/operation/Object_GETAccount */
+  implicit val accountReads: Reads[Queries.Account] = Reads { json =>
+    for {
+      id <- (json \ "Id").validate[String]
+      billToId <- (json \ "BillToId").validate[String]
+      soldToId <- (json \ "SoldToId").validate[String]
+      billCycleDay <- (json \ "BillCycleDay").validate[Int]
+      creditBalance <- (json \ "CreditBalance").validate[Float]
+      currency <- (json \ "Currency").validateOpt[String]
+      defaultPaymentMethodId <- (json \ "DefaultPaymentMethodId").validateOpt[String]
+      sfContactId <- (json \ "sfContactId__c").validateOpt[String]
+      paymentGateway <- (json \ "PaymentGateway").validateOpt[String]
+    } yield Queries.Account(
+      id = id,
+      billToId = billToId,
+      soldToId = soldToId,
+      billCycleDay = billCycleDay,
+      creditBalance = creditBalance,
+      currency = currency.flatMap(Currency.fromString),
+      defaultPaymentMethodId = defaultPaymentMethodId,
+      sfContactId = sfContactId,
+      paymentGateway = paymentGateway.flatMap(PaymentGateway.getByName),
+    )
+  }
+
+  /** @see https://developer.zuora.com/api-references/older-api/operation/Object_GETContact */
+  implicit val contactReads: Reads[Queries.Contact] = Reads { json =>
+    for {
+      id <- (json \ "Id").validate[String]
+      firstName <- (json \ "FirstName").validate[String]
+      lastName <- (json \ "LastName").validate[String]
+      postalCode <- (json \ "PostalCode").validateOpt[String]
+      country <- (json \ "Country").validateOpt[String]
+      email <- (json \ "WorkEmail").validateOpt[String]
+    } yield Queries.Contact(
+      id = id,
+      firstName = firstName,
+      lastName = lastName,
+      postalCode = postalCode,
+      country = country.flatMap(ZuoraLookup.country),
+      email = email,
+    )
+  }
+
+  /** @see https://developer.zuora.com/api-references/older-api/operation/Object_GETPaymentMethod */
+  implicit val paymentMethodReads: Reads[Queries.PaymentMethod] = Reads { json =>
+    for {
+      id <- (json \ "Id").validate[String]
+      paymentType <- (json \ "Type").validate[String]
+      numConsecutiveFailures <- (json \ "NumConsecutiveFailures").validateOpt[Int]
+      paymentMethodStatus <- (json \ "PaymentMethodStatus").validateOpt[String]
+      mandateId <- (json \ "MandateID").validateOpt[String]
+      tokenId <- (json \ "TokenId").validateOpt[String]
+      secondTokenId <- (json \ "SecondTokenId").validateOpt[String]
+      payPalEmail <- (json \ "PaypalEmail").validateOpt[String]
+      bankTransferType <- (json \ "BankTransferType").validateOpt[String]
+      bankTransferAccountName <- (json \ "BankTransferAccountName").validateOpt[String]
+      bankTransferAccountNumberMask <- (json \ "BankTransferAccountNumberMask").validateOpt[String]
+      bankCode <- (json \ "BankCode").validateOpt[String]
+      creditCardMaskNumber <- (json \ "CreditCardMaskNumber").validateOpt[String]
+      creditCardExpirationMonth <- (json \ "CreditCardExpirationMonth").validateOpt[Int]
+      creditCardExpirationYear <- (json \ "CreditCardExpirationYear").validateOpt[Int]
+      creditCardType <- (json \ "CreditCardType").validateOpt[String]
+    } yield Queries.PaymentMethod(
+      id = id,
+      mandateId = mandateId,
+      tokenId = tokenId,
+      secondTokenId = secondTokenId,
+      payPalEmail = payPalEmail,
+      bankTransferType = bankTransferType,
+      bankTransferAccountName = bankTransferAccountName,
+      bankTransferAccountNumberMask = bankTransferAccountNumberMask,
+      bankCode = bankCode,
+      `type` = paymentType,
+      creditCardNumber = creditCardMaskNumber.map(_.takeRight(4)),
+      creditCardExpirationMonth = creditCardExpirationMonth.map(_.toString),
+      creditCardExpirationYear = creditCardExpirationYear.map(_.toString),
+      creditCardType = creditCardType,
+      numConsecutiveFailures = numConsecutiveFailures,
+      paymentMethodStatus = paymentMethodStatus,
+    )
+  }
+
+  /** @see https://developer.zuora.com/api-references/api/operation/Action_POSTquery */
+  implicit val invoiceItemReads: Reads[Queries.InvoiceItem] = Reads { json =>
+    for {
+      id <- (json \ "Id").validate[String]
+      chargeAmount <- (json \ "ChargeAmount").validate[Float]
+      taxAmount <- (json \ "TaxAmount").validate[Float]
+      serviceStartDate <- (json \ "ServiceStartDate").validate[String]
+      serviceEndDate <- (json \ "ServiceEndDate").validate[String]
+      chargeNumber <- (json \ "ChargeNumber").validate[String]
+      productName <- (json \ "ProductName").validate[String]
+      subscriptionId <- (json \ "SubscriptionId").validate[String]
+    } yield Queries.InvoiceItem(
+      id = id,
+      price = chargeAmount + taxAmount,
+      serviceStartDate = new LocalDate(serviceStartDate),
+      serviceEndDate = new LocalDate(serviceEndDate),
+      chargeNumber = chargeNumber,
+      productName = productName,
+      subscriptionId = subscriptionId,
+    )
+  }
+}
