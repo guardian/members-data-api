c4e147835ef49d1172ee288c9e861c7f2aa1b4f2
Andrade <andradeolivier@gmail.com>
2026-07-15 22:28:57 +0100

    make zuora errors report their message instead of logging null

--- membership-common/src/main/scala/com/gu/zuora/models/Errors.scala
+++ membership-common/src/main/scala/com/gu/zuora/models/Errors.scala
@@ -2,6 +2,7 @@ package com.gu.zuora.models.errors
 
 sealed trait Error extends Throwable {
   val message: String
+  override def getMessage: String = message
 }
 
 case class QueryError(msg: String) extends Error {
